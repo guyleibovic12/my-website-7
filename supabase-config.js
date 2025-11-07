411e60a20463c29fde4703c92378120779606535
guyleibovic12 <guyleibovic12@gmail.com>
2025-11-07 16:30:27 +0200

    Update and rename firebase-config.js to supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.supabaseConfig = {
+  url: "https://qroxxyeljbxxbiupdeyu.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyb3h4eWVsamJ4eGJpdXBkZXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjQzNDQsImV4cCI6MjA3ODEwMDM0NH0.GAkSNfyTZpfhxiS8JAyCySINLxTZYwBkLfxGIhrcOcU"
+};
